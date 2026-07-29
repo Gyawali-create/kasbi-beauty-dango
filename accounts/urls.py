e72c88fa9e599f551ebf0f89edf2b9ff9eadbca3
Gyawali-Create <gyawalibeendu7396@gmail.com>
2026-07-29 09:07:09 +0545

    Working in the fbcombination

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,9 +5,9 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-    path('register/', views.register_view, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('register/', views.register_view, name='register'),
     path('profile/', views.profile_view, name='profile'),
     path('password-change/', auth_views.PasswordChangeView.as_view(
         template_name='accounts/password_change.html',
--- cart/urls.py
+++ cart/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'cart'
 
 urlpatterns = [
-    path('', views.cart_view, name='view'),
+    path('', views.cart_detail, name='detail'),
     path('add/<int:product_id>/', views.cart_add, name='add'),
     path('update/<int:item_id>/', views.cart_update, name='update'),
     path('remove/<int:item_id>/', views.cart_remove, name='remove'),
